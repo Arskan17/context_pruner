c1b70837adeaccb19ca768149657388c2e475270
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2025-09-30 16:07:18 +0200

    refactored for passages to be a list of sentences from the context.

--- data_formater.py
+++ data_formater.py
@@ -2,6 +2,7 @@ import json
 from concurrent.futures import ProcessPoolExecutor
 import os
 import uuid
+import re
 
 class DataFormatter:
 
@@ -9,28 +10,32 @@ class DataFormatter:
         pass
 
     def transform_file(self, input_line):
+        contexts = re.split(r'\. |\n\n', input_line.get("answer", ""))
         output_line = {
-            "answers": [input_line.get("answer", "")],
-            "passages": [],
+            "answers": [],
+            "passages": [{
+                    "is_selected":0,
+                    "url":"",
+                    "passage_text": context
+                } for context in contexts if context],
             "query": input_line.get("query", ""),
             "query_id": str(uuid.uuid4()),
-            "query_type": "Question_Answering",
-            "wellFormedAnswers": []
+            "query_type": "question_answering",
+            # "wellFormedAnswers": []
         }
 
         self.write_line_to_file(output_line)
 
     def write_line_to_file(self, line):
-        with open('german_rag_transformed_reformated1.jsonl', 'a', encoding='utf-8') as f:
+        with open('german_rag_transformed_reformated_MS-MARCO_v1.1.jsonl', 'a', encoding='utf-8') as f:
+            f.write(json.dumps(line, ensure_ascii=False) + '\n')
+
+    def write_line_to_file_cuncurrently(self, line):
+        with open('german_rag_transformed_reformated_cuncurrent_MS-MARCO_v1.1.jsonl', 'a', encoding='utf-8') as f:
             f.write(json.dumps(line, ensure_ascii=False) + '\n')
 
 if __name__ == "__main__":
     formatter = DataFormatter()
     
     with open('german_rag_transformed.jsonl', 'r', encoding='utf-8', errors='ignore') as file:
-        # lines = [json.loads(line) for line in file]
-        [formatter.transform_file(json.loads(line)) for line in file]
-    # with ProcessPoolExecutor() as executor:
-    #     executor.map(formatter.transform_file, lines)
-    # for line in lines:
-    #     formatter.transform_file(line)
+        [formatter.transform_file(json.loads(line)) for line in file if line.strip()]
